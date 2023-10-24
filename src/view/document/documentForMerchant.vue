825e1558c29ea83c204dd0c25da133eb4a9a43d5
luckyboy1004 <expert.vetted1024@gmail.com>
2023-10-24 12:33:47 +0300

    api document

---
+++ src/view/document/documentForMerchant.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="page">
+      <iframe src="/2.html" frameborder="0" width="100%" height="100%"></iframe>
+    </div>
+  </template>
+  
+  <script setup>
+  
+  defineOptions({
+    name: 'Dashboard'
+  })
+  
+  </script>  
+<style>
+  .page {
+    height: 75vh;
+  }
+</style>
